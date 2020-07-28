058921430fc58d2be297170820703377999068ef
Ishaq Ahmad Nizhami <Nizhami@Nizhamis-MacBook-Air.local>
2020-07-28 11:19:19 +0700

    allowed host www.urbanstadia.com

--- maestro/settings.py
+++ maestro/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = [
     'maestroproject.herokuapp.com',
     '127.0.0.1',
-    'urbanstadia.com']
+    'www.urbanstadia.com']
 
 
 # Application definition
